07f5e124b0c8ae6c3df382e57e5faf3eaf286ec3
alex <alex@nabu.be>
2016-05-31 20:28:40 +0200

    Added lister

---
+++ src/main/java/be/nabu/eai/module/http/server/HTTPServerInterfaceLister.java
@@ -0,0 +1,31 @@
+package be.nabu.eai.module.http.server;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import be.nabu.eai.developer.api.InterfaceLister;
+import be.nabu.eai.developer.util.InterfaceDescriptionImpl;
+
+public class HTTPServerInterfaceLister implements InterfaceLister {
+	
+	private static Collection<InterfaceDescription> descriptions = null;
+	
+	@Override
+	public Collection<InterfaceDescription> getInterfaces() {
+		if (descriptions == null) {
+			synchronized(HTTPServerInterfaceLister.class) {
+				if (descriptions == null) {
+					List<InterfaceDescription> descriptions = new ArrayList<InterfaceDescription>();
+					descriptions.add(new InterfaceDescriptionImpl("Virtual Host", "Request Rewriter", "be.nabu.eai.module.http.virtual.api.RequestRewriter.handle"));
+					descriptions.add(new InterfaceDescriptionImpl("Virtual Host", "Request Subscriber", "be.nabu.eai.module.http.virtual.api.RequestSubscriber.handle"));
+					descriptions.add(new InterfaceDescriptionImpl("Virtual Host", "Response Subscriber", "be.nabu.eai.module.http.virtual.api.ResponseSubscriber.handle"));
+					HTTPServerInterfaceLister.descriptions = descriptions;
+				}
+			}
+		}
+		return descriptions;
+	}
+
+
+}
